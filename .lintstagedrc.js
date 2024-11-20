6fa06c8038394169efcbd15272e885e43beb0e76
gbloch <gaetan.bloch@keyconsulting.fr>
2024-11-20 02:28:30 +0100

    style(shadcn): add shadcn/ui dep + import button component

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "*.{sh,js,jsx,ts,tsx,css,scss,md,htm,package.json}": [
     "prettier --write",
-    "eslint --fix",
     "git add",
   ],
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Hello from "@/app/components/hello";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  console.log("Hello");
+  console.log("Hello Server");
 
   return (
     <>
       <div className="text-3xl">Hello</div>
+      <Hello />
+      <Button>Click me</Button>
     </>
   );
 }
